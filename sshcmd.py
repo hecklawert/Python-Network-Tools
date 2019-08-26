f92b69c5cb1e88b03fd0729e441d583fe50223b0
Hëck Lawert <hectorgutifer@gmail.com>
2019-08-26 20:46:04 +0200

    SSH Tool with paramiko library

---
+++ sshcmd.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+'''
+
+*    @author      Hëck Lawert
+*    @githuh      https://github.com/hecklawert
+*    @date        26/08/2019
+*    @description SSH Tool with Paramiko
+
+'''
+
+import threading
+import paramiko
+import subprocess
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    #client.load_host_keys('/home/justin/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print ssh_session.recv(1024)
+    return
+
+ssh_command('192.168.1.50', 'heck', 'passwd','echo Hello World')
